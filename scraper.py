2e07904b3ab6da66ce20d1823911450c422cb649
hemanta212 <>
2022-05-16 09:10:51 +0000

    Update scraper cooldown

--- scraper.py
+++ scraper.py
@@ -1,10 +1,11 @@
+import time
 from news import get_ktm_votes, get_lalitpur_votes, get_bharatpur_votes
 
 def gen_message(full=False):
     city_data_map = dict(
-        Kathmandu=get_ktm_votes(),
-        Bharatpur=get_bharatpur_votes(),
-        Lalitpur=get_lalitpur_votes(),
+        Kathmandu=get_ktm_votes,
+        Bharatpur=get_bharatpur_votes,
+        Lalitpur=get_lalitpur_votes,
     ) 
     if not full:
         city_data_map = dict(Kathmandu=get_ktm_votes())
@@ -15,11 +16,13 @@ def gen_message(full=False):
     text = ''
     for city, data in city_data_map.items():
         text += construct_msg(city, data, full=full) if city!='Kathmandu' else construct_msg(city, data, concat_name=True, full=full)
+        time.sleep(2)
 
     submission_body = f"{header}\n{text}\n{footer}"
     return submission_body
 
 def construct_msg(city, data, concat_name=False, full=False):
+    data = data()
     mayor = f"**{city}**\n**Mayor**\n"
     get_name = lambda x: x['candidate-name'] if not concat_name else x['candidate-name'].split(' ')[0]
     candidates = [f"- {get_name(i)} = {i['vote-numbers']}" for i in data['mayor']]
