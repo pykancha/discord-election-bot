31a719468843f65445b4c18694c17ccf78ef6deb
hemanta212 <sharmahemanta.212@gmail.com>
2022-05-18 10:10:51 +0545

    Add on guild join, message send

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ from keep_alive import keep_alive
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-MY_GUILD = "Dev Server"
 #TODO
-# new channel entry
 # lead (+ lead) count
 # Graph and metadata
 
@@ -36,7 +34,7 @@ async def election_updater():
 
         full_updated_data = await election_info_updated(full=True)
         if full_updated_data:
-            await send_message(updated_data, to_me=True)
+            await send_message(full_updated_data, to_me=True)
         else:
             print("No Full updates")
 
@@ -45,6 +43,29 @@ async def election_updater():
         
         await asyncio.sleep(60)
 
+
+@client.event
+async def on_guild_join(guild):
+    print("Joining new guild", guild.name)
+    with open('ktm_data.json', 'r') as rf:
+        ktm_cache_data = json.load(rf)
+    with open('channelids.json', 'r') as rf:
+        channel_ids = json.load(rf)
+
+    embed_messages = gen_embed(ktm_cache_data)
+    for channel in guild.channels:
+        if channel.name == 'election-updates':
+            channel_ids.append(channel.id)
+            try:
+                for embed in embed_messages:
+                    await channel.send(embed=embed)
+                    print("New guild message sent")
+                    await asyncio.sleep(1)
+            except Exception as e:
+                print(e, channel.guild.name)
+
+    with open('channelids.json', 'w') as wf:
+        json.dump(channel_ids, wf)
 
 async def election_info_updated(full=False):
     data = get_city_data_map(full=full)
@@ -69,7 +90,6 @@ async def election_info_updated(full=False):
 
 async def send_message(data, to_me=False):
     embed_messages = gen_embed(data)
-    channels = []
     user_ids = [528240871481540611, 397648789793669121]
     if to_me:
         user_ids = user_ids[:1]
@@ -86,42 +106,22 @@ async def send_message(data, to_me=False):
         return
 
     print(len(client.guilds))
+    channels = []
     for guild in client.guilds:
         for channel in guild.channels:
-            channel.append(channels)
             if channel.name == 'election-updates':
+                channels.append(channel.id)
                 try:
                     for embed in embed_messages:
-                        await channel.send(embed)
+                        await channel.send(embed=embed)
                     await asyncio.sleep(1)
                 except Exception as e:
                     print(e, channel.guild.name)
 
-async def join_new_channel():
-    #with open('channelids.json', 'r') as rf:
-    #    channel_ids = json.load(rf)
-
-
-    #new_channel = False
-    #channels = []
-    #for channel in channels:
-    #    if channel.id not in channel_ids:
-    #        print("New channel", channel.guild.name)
-    #        new_channel = True
-    #    else:
-    #        new_channel = False
-
-    #    if outdated or new_channel:
-    #        print("Outdated:", outdated, "New channel:", new_channel)
-    #    else:
-    #        print("No new updates")
-
-        #with open('channelids.json', 'w') as wf:
-        #    ids = [i.id for i in channels]
-        #    json.dump(ids, wf)
-        #    
-    pass
-
+    # update channel ids
+    if channels:
+        with open('channelids.json', 'w') as wf:
+            json.dump(channels, wf)
 
 if __name__ == "__main__":
     keep_alive()
